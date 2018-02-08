2b2b580f127d8bff7ed78ce89256758eafcf2047
Aryna Karali <aryna.karali@itera.no>
2018-02-08 18:18:05 +0200

    accepted

---
+++ stack/20.ValidParentheses.js
@@ -0,0 +1,35 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ * 
+ * https://leetcode.com/problems/valid-parentheses/description/
+ * Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+ * The brackets must close in the correct order, "()" and "()[]{}" are all valid but "(]" and "([)]" are not.
+ */
+var isValid = function(s) {
+    var stack = [];
+    var sLength = s.length;
+    var open = ['(','{','['];
+    var close = [')','}',']'];
+    if (!open.includes(s[0])) {
+        return false;
+    }
+    for (let i = 0; i < sLength; i++) {
+        let curItem = s[i];
+        if (open.includes(curItem)) {
+            stack.push(curItem);
+        } 
+        if (close.includes(curItem)){
+            if (stack.length === 0){
+                return false;
+            }
+            let temp = stack.pop(); 
+            if ((temp ==='(' && s[i] ===')') || (temp ==='{' && s[i] ==='}') || (temp ==='[' && s[i] ===']')) {
+                continue;
+            } else {
+                return false;
+            }
+        }
+    }
+    return stack.length === 0 ? true : false;
+};
